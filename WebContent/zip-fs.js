dcf345fb4c47ada557df07671c45d078e954d8f0
William Ge <weix.ge@intel.com>
2013-12-07 10:24:22 +0800

    fix: onprocess will be called twice when signle file is small

--- WebContent/zip-fs.js
+++ WebContent/zip-fs.js
@@ -263,18 +263,18 @@
 		function stepCopy() {
 			var index = chunkIndex * CHUNK_SIZE;
 			if (onprogress){
-					onprogress(Math.min(index,reader.size), reader.size);
+					onprogress(Math.min(index+CHUNK_SIZE,reader.size), reader.size);
 			}
-			if (index < reader.size){
-				reader.readUint8Array(index, Math.min(CHUNK_SIZE, reader.size - index), function(array) {
-					writer.writeUint8Array(new Uint8Array(array), function() {
-						chunkIndex++;
-						stepCopy();
-					});
-				}, onerror);
-			}
-			else
-				writer.getData(onend);
+			reader.readUint8Array(index, Math.min(CHUNK_SIZE, reader.size - index), function(array) {
+				writer.writeUint8Array(new Uint8Array(array), function() {
+					chunkIndex++;
+					if (CHUNK_SIZE + index > reader.size) {
+						writer.getData(onend);
+						return ;
+					}
+					stepCopy();
+				});
+			}, onerror);
 		}
 
 		stepCopy();
